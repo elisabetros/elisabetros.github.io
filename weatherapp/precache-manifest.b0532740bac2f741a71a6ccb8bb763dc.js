ba604acbe682b85dd64a08aa695b72e875fc2bfb
elisabetros <elberoest@gmail.com>
2020-04-27 10:20:17 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.b0532740bac2f741a71a6ccb8bb763dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d872169d7d898fcc7cbd340bc67f02",
+    "url": "/elisabetros.github.io/weatherapp/index.html"
+  },
+  {
+    "revision": "63333d0e43a825f0a44c",
+    "url": "/elisabetros.github.io/weatherapp/static/css/main.98bc2c09.chunk.css"
+  },
+  {
+    "revision": "dbd15541945cd7f441e7",
+    "url": "/elisabetros.github.io/weatherapp/static/js/2.91121464.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/elisabetros.github.io/weatherapp/static/js/2.91121464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63333d0e43a825f0a44c",
+    "url": "/elisabetros.github.io/weatherapp/static/js/main.2497fd97.chunk.js"
+  },
+  {
+    "revision": "73f94b699f270a7aee8a",
+    "url": "/elisabetros.github.io/weatherapp/static/js/runtime-main.a1b1ac96.js"
+  }
+]);
